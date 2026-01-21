9d3733fe4b8cf5c98ee2e87aff00923d551a24aa
A3hi-ai <abhishekadhikari265@gmail.com>
2026-01-21 22:19:35 +0530

    Add files via upload

--- emp.sql
+++ emp.sql
@@ -1,23 +1,17 @@
-USE emp;
-
-CREATE TABLE emp (
-    sal INT,
-    job VARCHAR(30)
-);
-
-INSERT INTO emp VALUES ( 110000, 'MANAGER');
-INSERT INTO emp VALUES ( 50000, 'DEVELOPER');
-INSERT INTO emp VALUES ( 30000, 'ASI DEVELOPER');
-INSERT INTO emp VALUES (30000, 'ASI MANAGER');
-INSERT INTO emp VALUES ( 10000, 'HR');
-INSERT INTO emp VALUES ( 10000, 'TESTER');
+USE d1;
+CREATE TABLE emp (sal INT, job VARCHAR(30));
+INSERT INTO emp VALUES (30000, 'Manager');
+INSERT INTO emp VALUES (20000, "Developer");
+INSERT INTO emp VALUES (15000,'Tester');
+INSERT INTO emp VALUES (25800, 'HR');
+INSERT INTO emp VALUES (18000, 'Analyst');
 SELECT sal AS salary, job AS job_desc, sal * 12 AS annual_salary FROM emp;
-ALTER TABLE emp ADD department VARCHAR(30);
-describe emp;
-UPDATE emp SET department = 'IT' WHERE job = 'DEVELOPER';
-UPDATE emp SET department = 'IT' WHERE job = 'TESTER';
-UPDATE emp SET department = 'HR' WHERE job = 'HR';
-UPDATE emp SET department = 'Management' WHERE job = 'MANAGER';
-UPDATE emp SET department = 'Finance' WHERE job = 'Analyst';
-DESCRIBE EMP;
+ALTER TABLE emp ADD department VARCHAR (30);
+ALTER TABLE emp ADD department VARCHAR (30);
+UPDATE emp SET department = "Manager" WHERE job - "Developer";
+UPDATE emp SET department = "Developer" WHERE job = 'Tester';
+UPDATE emp SET department = 'Tester' WHERE Job = "HR";
+UPDATE emp SET department = "HR" WHERE job = 'Manager';
+UPDATE emp SET department = "Abalyst" WHERE job = 'dev';
 SELECT sal AS salary, job AS job_desc, department, sal * 12 AS annual_salary FROM emp;
+DESCRIBE emp;
